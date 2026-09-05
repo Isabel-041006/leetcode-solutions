3928f1bca5fa494a9f2548ea66a08a5ec52150de
Yutong Han <yutonghan@u.nus.edu>
2026-09-05 08:57:12 +0800

    Easy: 605. Can Place Flowers | Approach 1 | Runtime: 3 | Memory: 19576000

---
+++ 605. Can Place Flowers/Approach_1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        len_f = len(flowerbed)
+        if n == 0:
+            return True
+
+        for i in range(len_f):
+            if (flowerbed[i] == 0 
+                and (i ==0 or flowerbed[i-1] == 0) 
+                and (i == len_f - 1 or flowerbed[i+1] == 0)):
+                
+                flowerbed[i] = 1
+                n -=1
+                if n == 0:
+                    return True
+        return False
+
